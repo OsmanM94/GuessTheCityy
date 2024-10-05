3b8a6c4dff25879d6e170769b786832ea30f2080
Osman M <osmanmunur@yahoo.com>
2024-10-05 11:21:58 +0300

    Initial Commit

---
+++ GuessTheCity/GuessTheCityApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheCityApp.swift
+//  GuessTheCity
+//
+//  Created by asia on 05/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheCityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
